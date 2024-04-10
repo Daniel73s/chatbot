d873d1a4f527feeec7d80b2052d474ba0b046015
Daniel Luque <yodaaniel123@gmail.com>
2024-04-09 20:52:50 -0400

    chatbot con estilos

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const options = {
+    apiUrl: "https://api.opencopilot.so/backend",
+    socketUrl: "https://api.opencopilot.so",
+    initialMessage: "Hola soy maria en que te puedo ayudar?",
+    Text: "mariaaaa",
+    token: "afb6aed5478121de06c220bff100642a", // you can get your token from the dashboard
+    triggerSelector: "#triggerSelector",
+    language: "en", // the selector of the element that will trigger the copilot when clicked
+    containerProps: {
+      style: {
+        position: "fixed",
+        height: "100%",
+        bottom: "0",
+        right: "0",
+        width: "auto"
+      }
+    }
+
+  }
+  window.addEventListener("DOMContentLoaded", () => initAiCoPilot(options)); // window.onload
