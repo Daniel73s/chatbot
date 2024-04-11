12f0789e2d916fab47f82d95845ee62167e72c10
Daniel Luque <yodaaniel123@gmail.com>
2024-04-10 21:00:32 -0400

    cambiando el nombre de maria

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const options = {
     apiUrl: "https://api.opencopilot.so/backend",
     socketUrl: "https://api.opencopilot.so",
-    initialMessage: "Hola soy marIA en que te puedo ayudar?",
+    initialMessage: "Hola soy MarIA en que te puedo ayudar?",
     token: "afb6aed5478121de06c220bff100642a", // you can get your token from the dashboard
     triggerSelector: "#triggerSelector",
     language: "en", // the selector of the element that will trigger the copilot when clicked
